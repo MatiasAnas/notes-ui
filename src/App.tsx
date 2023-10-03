45034be8a37d89d72110bcc52709fc3a7392fd6c
Matias Anas <matias.anas@gmail.com>
2023-10-03 19:05:43 -0300

    feat: added scss modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,20 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
     <Box>
-      <AppBar position="static">
+      <AppBar position="static" className={styles.topbar}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Notes App
           </Typography>
         </Toolbar>
       </AppBar>
-      <Box className="appContent">
+      <Box className={styles.appContent}>
         <Typography variant="h6" component="div">
           App Content
         </Typography>
